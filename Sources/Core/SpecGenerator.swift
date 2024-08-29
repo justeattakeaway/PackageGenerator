3448c726f89ecfa1fbb17e17c81e43d0b90ae88c
Alberto De Bortoli <alberto.debortoli@justeattakeaway.com>
2024-08-29 09:38:20 +0100

    Address PR review

--- Sources/Core/SpecGenerator.swift
+++ Sources/Core/SpecGenerator.swift
@@ -5,14 +5,14 @@ import Yams
 
 /// Class to generate Specs models that can be used to ultimately generate `Package.swift` files.
 final class SpecGenerator {
-
+    
     enum GeneratorError: Error {
         case invalidFormat(String)
     }
-
+    
     let specUrl: URL
     let dependenciesUrl: URL
-
+    
     /// The default initializer.
     ///
     /// - Parameters:
@@ -29,22 +29,22 @@ final class SpecGenerator {
     func makeSpec() throws -> Spec {
         let spec: Spec = try decodeModel(from: specUrl)
         let dependencies: Dependencies = try decodeModel(from: dependenciesUrl)
-
+        
         let mappedDependencies: [Spec.RemoteDependency] = spec.remoteDependencies
             .compactMap { remoteDependency -> Spec.RemoteDependency? in
-            guard let dependency = dependencies.dependencies.first(where: {
-                $0.name == remoteDependency.name
-            }) else {
-                return nil
-            }
+                guard let dependency = dependencies.dependencies.first(where: {
+                    $0.name == remoteDependency.name
+                }) else {
+                    return nil
+                }
                 return Spec.RemoteDependency(
-                name: dependency.name,
-                url: remoteDependency.url ?? dependency.url,
-                version: remoteDependency.version ?? dependency.version,
-                revision: remoteDependency.revision ?? dependency.revision,
-                branch: remoteDependency.branch ?? dependency.branch
-            )
-        }
+                    name: dependency.name,
+                    url: remoteDependency.url ?? dependency.url,
+                    version: remoteDependency.version ?? dependency.version,
+                    revision: remoteDependency.revision ?? dependency.revision,
+                    branch: remoteDependency.branch ?? dependency.branch
+                )
+            }
         
         return Spec(
             name: spec.name,
@@ -59,7 +59,7 @@ final class SpecGenerator {
             swiftLanguageVersions: spec.swiftLanguageVersions
         )
     }
-
+    
     private func decodeModel<T: Decodable>(from url: URL) throws -> T {
         let specData = try Data(contentsOf: url)
         switch url.pathExtension {
