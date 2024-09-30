f156353b8f604e956479b5f59984bfed5d46c28a
Antonio Strijdom <36736031+antoniostrijdom@users.noreply.github.com>
2024-09-30 09:29:06 +0100

    Replaced swiftLanguageVersions with swiftLanguageModes for Swift 6 onwards (#8)
    ---------
    
    Co-authored-by: Antonio Strijdom <antonio.strijdom@justeattakeaway.com>

--- Sources/Core/Spec+Context.swift
+++ Sources/Core/Spec+Context.swift
@@ -33,6 +33,14 @@ extension Spec.RemoteDependency {
 }
 
 extension Spec {
+    var swiftToolsVersionMajor: Int? {
+        guard let swiftToolsVersionString = swiftToolsVersion else { return nil }
+        if let majorString = swiftToolsVersionString.components(separatedBy: ".").first {
+            return Int(majorString)
+        }
+        return nil
+    }
+    
     func makeContext() -> [String: Any] {
         let values: [String: Any?] = [
             "package_name": name,
@@ -44,6 +52,7 @@ extension Spec {
             "local_binary_targets": localBinaryTargets,
             "remote_binary_targets": remoteBinaryTargets,
             "swift_tools_version": swiftToolsVersion,
+            "swift_tools_version_major": swiftToolsVersionMajor,
             "swift_versions": swiftLanguageVersions
         ]
         return values.compactMapValues { $0 }
