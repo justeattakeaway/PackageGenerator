70e9939c13902e0b86ec21a48ad150f5578c9fc4
Alberto De Bortoli <albertodebortoli@gmail.com>
2025-01-20 13:55:58 +0000

    Update swift-argument-parser to 1.5.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,7 +10,7 @@ let package = Package(
         .executable(name: "PackageGenerator", targets: ["PackageGenerator"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit", from: "2.8.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.3.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6")
