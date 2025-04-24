4c757889c32001eb1ff561e4806bac14b88c0ef2
Alberto De Bortoli <albertodebortoli@gmail.com>
2025-04-24 13:35:08 +0100

    Update PackageGeneratorTests to test the Generator struct

--- Tests/PackageGeneratorTests.swift
+++ Tests/PackageGeneratorTests.swift
@@ -4,7 +4,7 @@ import XCTest
 @testable import PackageGenerator
 
 final class PackageGeneratorTests: XCTestCase {
-
+    
     enum PackageType: String {
         case revisionProduct = "RevisionProduct"
         case branchProduct = "BranchProduct"
@@ -18,88 +18,101 @@ final class PackageGeneratorTests: XCTestCase {
         case dependenciesAsBinaryTargets = "DependenciesAsBinaryTargets"
         case dependenciesAsBinaryTargetsWithExclusions = "DependenciesAsBinaryTargetsWithExclusions"
     }
-
+    
     let resourcesFolder = URL(fileURLWithPath: #filePath)
         .deletingLastPathComponent()
         .appendingPathComponent("Resources")
-
+    
     lazy var packagesFolderUrl = resourcesFolder.appendingPathComponent("Packages")
     lazy var dependenciesFilename = "TestDependencies"
     lazy var versionRefs = "TestVersionRefs"
-    lazy var templatePath = resourcesFolder.appendingPathComponent("Package.stencil")
-
-    func test_SingleProduct() throws {
-        try assertPackage(for: .singleProduct)
+    lazy var templateUrl = resourcesFolder.appendingPathComponent("Package.stencil")
+    
+    private let fileManager = FileManager.default
+    
+    func test_SingleProduct() async throws {
+        try await assertPackage(for: .singleProduct)
     }
-
-    func test_RevisionProduct() throws {
-        try assertPackage(for: .revisionProduct)
+    
+    func test_RevisionProduct() async throws {
+        try await assertPackage(for: .revisionProduct)
     }
-
-    func test_BranchProduct() throws {
-        try assertPackage(for: .branchProduct)
+    
+    func test_BranchProduct() async throws {
+        try await assertPackage(for: .branchProduct)
     }
-
-    func test_MultipleProducts() throws {
-        try assertPackage(for: .multipleProducts)
+    
+    func test_MultipleProducts() async throws {
+        try await assertPackage(for: .multipleProducts)
     }
-
-    func test_customPlatforms() throws {
-        try assertPackage(for: .customPlatforms)
+    
+    func test_customPlatforms() async throws {
+        try await assertPackage(for: .customPlatforms)
     }
-
-    func test_DependencyVersionOverride() throws {
-        try assertPackage(for: .dependencyOverride)
+    
+    func test_DependencyVersionOverride() async throws {
+        try await assertPackage(for: .dependencyOverride)
     }
-
-    func test_complexTargets() throws {
-        try assertPackage(for: .complexTargets)
+    
+    func test_complexTargets() async throws {
+        try await assertPackage(for: .complexTargets)
     }
-
-    func test_executableProduct() throws {
-        try assertPackage(for: .executableProduct)
+    
+    func test_executableProduct() async throws {
+        try await assertPackage(for: .executableProduct)
     }
-
-    func test_pluginProduct() throws {
-        try assertPackage(for: .plugins)
+    
+    func test_pluginProduct() async throws {
+        try await assertPackage(for: .plugins)
     }
-
+    
     func test_dependenciesAsBinaryTargets() async throws {
         try await assertConvertedPackage(for: .dependenciesAsBinaryTargets, exclusions: [])
     }
-
+    
     func test_dependenciesAsBinaryTargetsWithExclusions() async throws {
         try await assertConvertedPackage(for: .dependenciesAsBinaryTargetsWithExclusions, exclusions: ["LocalDependencyA"])
     }
-
-    private func assertPackage(for packageType: PackageType) throws {
+    
+    private func assertPackage(for packageType: PackageType) async throws {
         for `extension` in ["json", "yml"] {
             let specUrl = resourcesFolder
                 .appendingPathComponent("Packages")
                 .appendingPathComponent(packageType.rawValue)
                 .appendingPathComponent(packageType.rawValue)
                 .appendingPathExtension(`extension`)
-
-            let packageUrl = resourcesFolder
+            
+            let fixturePackageUrl = resourcesFolder
                 .appendingPathComponent("Packages")
                 .appendingPathComponent(packageType.rawValue)
                 .appendingPathComponent("Package")
                 .appendingPathExtension("swift")
-
+            
             let dependenciesUrl = resourcesFolder
                 .appendingPathComponent(dependenciesFilename)
                 .appendingPathExtension("yml")
-
-            let spec = try SpecGenerator().makeSpec(specUrl: specUrl, dependenciesUrl: dependenciesUrl)
-            let templater = Templater(templateUrl: templatePath)
-            let packageContent = try templater.renderTemplate(context: spec.makeContext())
-
-            let expectedPackageContent = try String(contentsOf: packageUrl)
-
-            XCTAssertEqual(packageContent, expectedPackageContent)
+            
+            let generator = Generator(
+                templateUrl: templateUrl,
+                dependenciesUrl: dependenciesUrl,
+                dependencyFinder: MockDependencyFinder(),
+                writer: MockWriter()
+            )
+            
+            let sutPackageUrl = try await generator.generatePackage(
+                at: fileManager.temporaryDirectory,
+                filename: "Package.swift",
+                specUrl: specUrl,
+                dependencyTreatment: .standard
+            )
+            
+            let sutPackageContent = try String(contentsOf: sutPackageUrl)
+            let expectedPackageContent = try String(contentsOf: fixturePackageUrl)
+            
+            XCTAssertEqual(sutPackageContent, expectedPackageContent)
         }
     }
-
+    
     private func assertConvertedPackage(for packageType: PackageType, exclusions: [String]) async throws {
         for `extension` in ["json", "yml"] {
             let specUrl = resourcesFolder
@@ -107,51 +120,43 @@ final class PackageGeneratorTests: XCTestCase {
                 .appendingPathComponent(packageType.rawValue)
                 .appendingPathComponent(packageType.rawValue)
                 .appendingPathExtension(`extension`)
-
-            let packageUrl = resourcesFolder
+            
+            let fixturePackageUrl = resourcesFolder
                 .appendingPathComponent("Packages")
                 .appendingPathComponent(packageType.rawValue)
                 .appendingPathComponent("Package")
                 .appendingPathExtension("swift")
-
+            
             let dependenciesUrl = resourcesFolder
                 .appendingPathComponent(dependenciesFilename)
                 .appendingPathExtension("yml")
-
+            
             let versionRefsUrl = resourcesFolder
                 .appendingPathComponent(versionRefs)
                 .appendingPathExtension("json")
-
-            let spec = try SpecGenerator().makeSpec(specUrl: specUrl, dependenciesUrl: dependenciesUrl)
-            let templater = Templater(templateUrl: templatePath)
-            let packageContent = try templater.renderTemplate(context: spec.makeContext())
-
-            let tmpFilename = "tmp_\(UUID().uuidString)_Package.swift"
-            let tmpPackageUrl = try saveFileToTempDirectory(filename: tmpFilename, content: packageContent)
-
-            let packageConvertor = PackageConvertor()
-            let convertedSpec = try await packageConvertor.convertDependenciesToBinaryTargets(
+            
+            let generator = Generator(
+                templateUrl: templateUrl,
+                dependenciesUrl: dependenciesUrl,
                 dependencyFinder: MockDependencyFinder(),
-                spec: spec,
-                packageFilePath: tmpPackageUrl.path(),
-                relativeDependenciesPath: "../.xcframeworks",
-                versionRefsPath: versionRefsUrl.path(),
-                exclusions: exclusions
+                writer: MockWriter()
             )
-
-            let convertedPackageContent = try templater.renderTemplate(context: convertedSpec.makeContext())
-
-            let expectedPackageContent = try String(contentsOf: packageUrl)
-
-            XCTAssertEqual(convertedPackageContent, expectedPackageContent)
+            
+            let sutPackageUrl = try await generator.generatePackage(
+                at: fileManager.temporaryDirectory,
+                filename: "tmp_\(UUID().uuidString)_Package.swift",
+                specUrl: specUrl,
+                dependencyTreatment: .binaryTargets(
+                    relativeDependenciesPath: "../.xcframeworks",
+                    versionRefsPath: versionRefsUrl.path(),
+                    exclusions: exclusions
+                )
+            )
+            
+            let sutPackageContent = try String(contentsOf: sutPackageUrl)
+            let expectedPackageContent = try String(contentsOf: fixturePackageUrl)
+            
+            XCTAssertEqual(sutPackageContent, expectedPackageContent)
         }
-    }
-
-    private func saveFileToTempDirectory(filename: String, content: Content) throws -> URL {
-        let tempDirectory = FileManager.default.temporaryDirectory
-        let fileURL = tempDirectory
-            .appendingPathComponent(filename)
-        try content.write(to: fileURL, atomically: true, encoding: .utf8)
-        return fileURL
     }
 }
